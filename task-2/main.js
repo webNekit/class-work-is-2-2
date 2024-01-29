a171d508c059e7a827609a8c4b8635090ab52f1a
webNekit <mochalov-hunter@mail.ru>
2024-01-29 13:35:38 +0300

    update

--- task-2/main.js
+++ task-2/main.js
@@ -1,5 +1,5 @@
 // одномерный массив
 let nameArray = ["Tom", "Harry", "Ron", "Bob"];
 
-let studentsArray = [ ["Степан", "Володя", "Андрей"], ["Даша","Катя","Ирина"], [23, true, 7.75] ];
+let studentsArray = [ ["Степан", "Володя", "Андрей12321"], ["Даша","Катя","Ирина"], [23, true, 7.75] ];
 console.log(studentsArray[1][1]);
